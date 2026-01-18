04b6a82a9c7370ea7c6e45ecf35b0eeb98ef9420
Benny Nottonson <bennynottonson@gmail.com>
2026-01-17 19:31:06 -0500

    Avatars
    Signed-off-by: Benny Nottonson <bennynottonson@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,39 @@
 import styles from "./page.module.css";
 
+const founders = [
+  {
+    name: "Benjamin Abix",
+    avatar: "/benjamin-avatar.jpg",
+  },
+  {
+    name: "Lucielle Abix",
+    avatar: "/lucielle-avatar.jpg",
+  },
+];
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.title}>Abix</h1>
         <div className={styles.founders}>
-          <div className={styles.person}>
-            <div className={styles.avatar} />
-            <span className={styles.name}>Benjamin Nottonson-Abix</span>
-          </div>
-          <div className={styles.person}>
-            <div className={styles.avatar} />
-            <span className={styles.name}>Lucy Nottonson-Abix</span>
-          </div>
+          {founders.map((founder) => (  
+            <div key={founder.name} className={styles.person}>
+              <img
+                src={founder.avatar}
+                alt={founder.name}
+                className={styles.avatar}
+              />
+              <span className={styles.name}>{founder.name}</span>
+            </div>
+          ))}
         </div>
       </main>
     </div>
   );
 }
+
+export const metadata = {
+  title: "Abix",
+  description: "Abix",
+};
